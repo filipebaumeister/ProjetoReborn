f976b7dea7d38abcc953aaa187e3aaa86fda8959
Bruno A Souto <bruno.drd@gmail.com>
2016-11-22 23:43:26 +0000

    Subindo as Migrations

--- db/migrate/20161028023727_create_record_financials.rb
+++ db/migrate/20161028023727_create_record_financials.rb
@@ -1,8 +1,8 @@
 class CreateRecordFinancials < ActiveRecord::Migration
   def change
     create_table :record_financials do |t|
-      #t.references :FINANCIAL, index: true, foreign_key: true
-      #t.references :RECORD, index: true, foreign_key: true
+      t.references :FINANCIAL, index: true, foreign_key: true
+      t.references :RECORD, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20161031195223_create_financials.rb
+++ db/migrate/20161031195223_create_financials.rb
@@ -2,9 +2,9 @@ class CreateFinancials < ActiveRecord::Migration
   def change
     create_table :financials do |t|
       t.string :NATURE
-      #t.references :CUSTOMERPROVIDER, index: true, foreign_key: true
+      t.references :CUSTOMERPROVIDER, index: true, foreign_key: true
       t.boolean :PAID
-      # t.references :BANK, index: true, foreign_key: true
+      t.references :BANK, index: true, foreign_key: true
       t.date :DATE_EMISSION
       t.date :DATE_DUE
       t.date :DATE_PAYMENT
@@ -13,7 +13,7 @@ class CreateFinancials < ActiveRecord::Migration
       t.decimal :MULCT
       t.decimal :INTEREST
       t.decimal :VALUE_PAID
-      # t.references :RECORD, index: true, foreign_key: true
+      t.references :RECORD, index: true, foreign_key: true
       t.text :HISTORY
 
       t.timestamps null: false
