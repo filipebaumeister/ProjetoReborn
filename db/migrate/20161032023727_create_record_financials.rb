8f242ef2d93a7c3e6191390eacd1878f545d4afd
Bruno A Souto <bruno.drd@gmail.com>
2016-12-05 17:21:40 +0000

    2 - Validacao Migracao PostGreSql Heroku e C9

--- db/migrate/20161032023727_create_record_financials.rb
+++ db/migrate/20161032023727_create_record_financials.rb
@@ -1,8 +1,8 @@
 class CreateRecordFinancials < ActiveRecord::Migration
   def change
     create_table :record_financials do |t|
-      t.references :financial, index: true, foreign_key: true
-      t.references :record, index: true, foreign_key: true
+      t.references :financials, index: true, foreign_key: true
+      t.references :records, index: true, foreign_key: true
 
       t.timestamps null: false
     end
