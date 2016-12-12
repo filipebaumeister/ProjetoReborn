59afeba489350c415823bd70d4060475e87b0e8d
Bruno A Souto <bruno.drd@gmail.com>
2016-12-12 14:27:22 +0000

    ChavesRebuild

--- db/migrate/20161212142629_add_chaves_to_rebuilds.rb
+++ db/migrate/20161212142629_add_chaves_to_rebuilds.rb
@@ -1,4 +1,4 @@
-class AddChavesToRebuild < ActiveRecord::Migration
+class AddChavesToRebuilds < ActiveRecord::Migration
   def change
     add_reference :rebuilds, :car, index: true, foreign_key: true
   end
